cfc923ed7bf1b509d2c4d5d96c798479854365c5
Sajib Hossain <sajibhossain1185@gmail.com>
2022-07-03 04:27:17 +0600

    finished contact right

--- components/home/contact/ContactRight.jsx
+++ components/home/contact/ContactRight.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import { FaLinkedinIn } from 'react-icons/fa'
+import { AiOutlineTwitter } from 'react-icons/ai'
 
 const ContactRight = () => {
     return (
-        <div className="contact__right"></div>
+        <div className="contact__right">
+            <div className="info">
+                <h3>Contact Info</h3>
+                <div>
+                    <p>sajibhossain1185@gmail.com</p>
+                    <p>+8801782651286</p>
+                </div>
+            </div>
+            <div className='social__media'>
+                <h3>social media</h3>
+                <div>
+                    <a href="#" className="social__link">
+                        <FaLinkedinIn />
+                    </a>
+                    <a href="#" className="social__link">
+                        <AiOutlineTwitter />
+                    </a>
+                </div>
+            </div>
+        </div>
     )
 }
 
