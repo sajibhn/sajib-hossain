9641905ee27c452ad6357b48d624006530c7182f
Sajib Hossain <sajibhossain1185@gmail.com>
2022-07-03 00:39:50 +0600

    finished header

---
+++ components/global/Header.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+import { BiGridAlt } from 'react-icons/bi'
+
+const Header = () => {
+    const [menuOpen, setMenuOpen] = useState(false)
+    return (
+        <header className={`${menuOpen ? 'header active' : 'header'}`}>
+            <nav className="nav container">
+                <div className="nav__logo">
+                    <h3>Saj</h3>
+                </div>
+                <div className="nav__menu">
+                    <ul className="nav__list">
+                        <li className="nav__item"><a href="#" className="nav__link">Home</a></li>
+                        <li className="nav__item"><a href="#" className="nav__link">Projects</a></li>
+                        <li className="nav__item"><a href="#" className="nav__link">Contact</a></li>
+                    </ul>
+                </div>
+                <div className="nav__resume">
+                    <button className="resume">Download CV</button>
+                </div>
+                <div className="nav__toggle" onClick={() => setMenuOpen(!menuOpen)}>
+                    <BiGridAlt />
+                </div>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,10 @@
+import Header from '../components/global/Header'
 import '../styles/main.scss'
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <Header />
+    <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
