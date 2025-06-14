ac7c4462d2b9a82d0a57779d7dde1df7d77c97e1
Sajib Hossain <sajibhossain1185@gmail.com>
2025-06-14 22:30:10 +0600

    verify

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,7 @@ export default function Document() {
         <Html>
             <Head>
                 <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
+                <meta name="google-site-verification" content="ChArkkyrP8fbcZ72QtcTb27hIv6M_TjXU1GMtBb-FMI" />
             </Head>
             <body>
                 <Main />
